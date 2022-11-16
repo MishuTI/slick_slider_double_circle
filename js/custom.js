a2c8da461210f14eb0278c043f732ed3de04d861
MishuTI <114094037+MishuTI@users.noreply.github.com>
2022-11-16 08:28:45 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$('.slider_part').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrow:false,
+
+    prevArrow:'<i class="fa-solid fa-chevron-left left"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-right right"></i>',
+    dots:true,
+  });
+              
